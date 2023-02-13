e55b3ef4f2168b4152690eed0835a697730d403c
Érik Eiji <eriknibe1@gmail.com>
2023-02-13 14:01:23 -0300

    initial commit

---
+++ src/database/config.ts
@@ -0,0 +1,9 @@
+import client from "./connection";
+
+const connectDatabase = async (): Promise<void> => {
+    await client.connect();
+
+    console.log("Database connected!")
+};
+
+export default connectDatabase;
---
+++ src/routers/users.routers.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+
+const userRoutes: Router = Router();
+
+
+
+export default userRoutes;
